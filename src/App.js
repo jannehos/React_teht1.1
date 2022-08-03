5073e0847245d29a6696a16e26c5485c7e60e9e7
janne.hostikka <janne.hostikka@awacademy.training>
2022-08-03 08:09:05 +0300

    react_1.3valmis

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ const Header = (props) =>{
   const Content = (props) =>{
   return(
     <div>
-    <Part osat={props.parts[0]} harjoitus={props.exercises[0]} />
+    <Part osat={props.parts[0]}  harjoitus={props.exercises[0]} />
   
     <Part osat={props.parts[1]} harjoitus={props.exercises[1]} />
   
@@ -74,8 +74,6 @@ const Header = (props) =>{
   )
   }
   const Part = (props) => {
-  
-    //console.log(props); / Tähän kohtaan kun halutaan tulostaa senhetkisen propsin sisältö
   
     return(
   
@@ -110,9 +108,8 @@ const Header = (props) =>{
         <Header course={course}/>
       
         
-        <Part osat={part1 + exercises1}/>
-        <Part osat={part2 + exercises2}/>
-        <Part osat={part3 + exercises3}/>
+        <Content parts={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
+
   
         <Total inTot={exercises1 + exercises2 + exercises3}/>
   
@@ -124,6 +121,77 @@ const Header = (props) =>{
 
   // ************************************************************************************************teht.1.2
 */
+const Header = (props) =>{
+  return (
+  <div>
+  <h1>{props.course}</h1>
+  
+  </div>
+  )
+  }
+  const Content = (props) =>{
+  return(
+    <div>
+    <Part osat={props.parts[0]}  harjoitus={props.exercises[0]} />
+  
+    <Part osat={props.parts[1]} harjoitus={props.exercises[1]} />
+  
+    <Part osat={props.parts[2]} harjoitus={props.exercises[2]} />
+  
+  </div>
+  )
+  }
+  const Part = (props) => {
+  
+    return(
+  
+      <div>
+  
+        {props.osat} {props.harjoitus}
+  
+      </div>
+  
+    )
+    }
+  
+  const Total = (props) =>{
+    return(
+  
+  <p>Number of exercises: {props.inTot}</p>
+  
+    )
+  }
+  
+  const App = () => {
 
+    const course = 'Half Stack application development'
+    const part1 = {
 
+      name: 'Fundamentals of React',
+      exercises: 10
+    }
+    const part2 = {
 
+      name: 'Using props to pass data',
+      exercises: 7
+    }
+    const part3 = {
+
+      name: 'State of a component',
+      exercises: 14
+    }
+  
+  
+    return (
+      <div>
+        <h1>{course}</h1>
+	  <p>{part1.name}: {part1.exercises}</p>
+	  <p>{part2.name}: {part2.exercises}</p>
+	  <p>{part3.name}: {part3.exercises}</p>
+	  <p>Number of exercises: {part1.exercises + part2.exercises + part3.exercises}</p>
+
+      </div>
+    )
+  }
+  
+  export default App
